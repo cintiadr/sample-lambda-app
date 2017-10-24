7468292f4fbc76d7024ba236c20a922e9800e098
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-10-24 19:28:08 +1100

    Add more tests

--- spec/hello_world_spec.js
+++ spec/hello_world_spec.js
@@ -1,11 +1,11 @@
 var request = require("request");
 
-var base_url = "https://" + process.env.ENVIRONMENT + ".cintia.me/"
+var base_url = "https://" + process.env.ENVIRONMENT + ".cintia.me"
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
+      request.get(base_url + "/", function(error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
       });
@@ -18,4 +18,37 @@ describe("Hello World Server", function() {
       });
     });
   });
+
+  describe("GET /healthcheck", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("returns healthy kale", function(done) {
+      request.get(base_url + "/healthcheck", function(error, response, body) {
+        expect(body).toBe("Super healthy Kale!");
+        done();
+      });
+    });
+  });
+
+  describe("GET /metadata", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    // it("returns healthy kale", function(done) {
+    //   request.get(base_url + "/healthcheck", function(error, response, body) {
+    //     expect(body).toBe("Super healthy Kale!");
+    //     done();
+    //   });
+    // });
+  });
+
 });
